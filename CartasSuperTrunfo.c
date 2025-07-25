73c83ea703026c89ed67837738d65e4c3ab5ff71
MarceloBernardesDaSilva <marcelo_volei15@hotmail.com>
2025-07-25 17:00:59 +0000

    atualizacao 25-07

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,9 +4,21 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+//Teste Marcelo Bernardes da Silva
 
 int main() {
+    printf("Desafio - Cartas Super Trunfo!\n")
+    
+
+
+
+
+
+
+
+
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
